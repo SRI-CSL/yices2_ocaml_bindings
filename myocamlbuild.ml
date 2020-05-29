4897be961d77e1fd0d88d44c0256286ebbeb4a76
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2020-05-28 19:23:59 -0700

    fixed build

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,8 +1,14 @@
 open Ocamlbuild_plugin
 
+let dummy_gmp env _build =
+  tag_any ["gmp"];
+  Nop
+
+let dummy_nogmp env _build =
+  Nop
+
 let gmp_true path env _build =
   let arg = env (Pathname.concat path "gmp.mlh") in
-  tag_any ["gmp"];
   Echo(["[%%define gmp_present true]"], arg)
 
 let gmp_false path env _build =
@@ -28,11 +34,14 @@ let () = dispatch begin function
       rule "src_tests/gmp false" ~prod:"src_tests/gmp.mlh" ~insert:(`top) (gmp_false "src_tests");
       rule "src_tests/gmp true"  ~prod:"src_tests/gmp.mlh" ~dep:"gmp.o" ~insert:(`top) (gmp_true "src_tests");
       rule "meta nogmp"  ~prod:"src/META" ~dep:"src/META_nogmp" ~insert:(`top) meta_nogmp;
-      rule "meta gmp"    ~prod:"src/META" ~deps:["src/gmp.o";"src/META_gmp"] ~insert:(`top) meta_gmp;
+      rule "meta gmp"    ~prod:"src/META" ~deps:["gmp.o";"src/META_gmp"] ~insert:(`top) meta_gmp;
+      rule "dummy nogmp"  ~prod:"dummy" ~insert:(`top) dummy_nogmp;
+      rule "dummy gmp"    ~prod:"dummy" ~dep:"gmp.o" ~insert:(`top) dummy_gmp;
       dep ["ocaml"; "ocamldep"; "main"] ["src/gmp.mlh"];
       dep ["ocaml"; "compile"; "main"] ["src/gmp.mlh"];
       dep ["ocaml"; "ocamldep"; "tests"] ["src_tests/gmp.mlh"];
       dep ["ocaml"; "compile"; "tests"] ["src_tests/gmp.mlh"];
+      dep ["ocaml"] ["dummy"];
       flag ["ocaml"; "compile"; "gmp"] (S [A "-package"; A "zarith"; A "-package"; A "ctypes-zarith"]);
       flag ["ocaml"; "link"; "gmp"] (S [A "-package"; A "zarith"; A "-package"; A "ctypes-zarith"])
     | _ -> ()
