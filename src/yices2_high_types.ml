31e970843bbd277ce65d64a809999440866131e8
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2020-08-04 18:24:42 -0700

    better high types

--- src/yices2_high_types.ml
+++ src/yices2_high_types.ml
@@ -131,7 +131,7 @@ module Types = struct
   type rational = unit
   [%%endif]
   type 'a termstruct =
-    | A0 : [ `YICES_BOOL_CONSTANT
+    | A0 : [< `YICES_BOOL_CONSTANT
            | `YICES_ARITH_CONSTANT
            | `YICES_BV_CONSTANT
            | `YICES_ARITH_ROOT_ATOM 
@@ -139,12 +139,12 @@ module Types = struct
            | `YICES_VARIABLE
            | `YICES_UNINTERPRETED_TERM ]
            * term_t                        -> [`a0] termstruct
-    | A1 : [ `YICES_NOT_TERM
+    | A1 : [< `YICES_NOT_TERM
            | `YICES_ABS
            | `YICES_CEIL 
            | `YICES_FLOOR
            | `YICES_IS_INT_ATOM ] * term_t -> [`a1] composite termstruct
-    | A2 : [ `YICES_EQ_TERM 
+    | A2 : [< `YICES_EQ_TERM 
            | `YICES_BV_ASHR 
            | `YICES_BV_DIV 
            | `YICES_BV_GE_ATOM 
@@ -162,17 +162,17 @@ module Types = struct
            | `YICES_RDIV ]
            * term_t * term_t         -> [`a2] composite termstruct
     | ITE : term_t * term_t * term_t -> [`a3] composite termstruct
-    | Astar : [ `YICES_TUPLE_TERM
+    | Astar : [< `YICES_TUPLE_TERM
               | `YICES_DISTINCT_TERM 
               | `YICES_OR_TERM 
               | `YICES_XOR_TERM
               | `YICES_BV_ARRAY ] * term_t list -> [`astar] composite termstruct
-    | Bindings : { c    : [ `YICES_FORALL_TERM | `YICES_LAMBDA_TERM ];
-                   vars : term_t list;
-                   body : term_t }              -> [`bindings] composite termstruct
-    | App    : term_t * term_t list             -> [`app]      composite termstruct
-    | Update : { array : term_t; index : term_t list; value : term_t}      -> [`update] composite termstruct
-    | Projection : [ `YICES_SELECT_TERM | `YICES_BIT_TERM ] * int * term_t -> [`projection] termstruct
+    | Bindings  : { c    : [< `YICES_FORALL_TERM | `YICES_LAMBDA_TERM ];
+                    vars : term_t list;
+                    body : term_t }              -> [`bindings] composite termstruct
+    | App       : term_t * term_t list           -> [`app]      composite termstruct
+    | Update    : { array : term_t; index : term_t list; value : term_t}    -> [`update] composite termstruct
+    | Projection : [< `YICES_SELECT_TERM | `YICES_BIT_TERM ] * int * term_t -> [`projection] termstruct
     | BV_Sum    : (bool list * (term_t option)) list -> [`bvsum] termstruct
     | Sum       : (rational * (term_t option)) list   -> [`sum]   termstruct
     | Product   : bool * (term_t * uint) list    -> [`prod]  termstruct
