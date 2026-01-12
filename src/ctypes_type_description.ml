23026abad5aef01427647665dd21030c3d989925
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2026-01-12 14:05:25 -0800

    Compile-time test for Yices version

--- src/ctypes_type_description.ml
+++ src/ctypes_type_description.ml
@@ -132,110 +132,215 @@ module Types (F : TYPE) = struct
     end
   let type_vector_t = typedef type_vector_s_0 "type_vector_t"
 
-  let term_constructor = 
+  [%%if defined yices_ge_2_7]
+  let term_constructor =
    (* prerr_endline "[ctypes_type_description.ml] term_constructor" ; *)
     let (to_int, of_int) =
       ((function
-        | `YICES_CONSTRUCTOR_ERROR -> (-1L)
-        | `YICES_BOOL_CONSTANT -> 0L
-        | `YICES_ARITH_CONSTANT -> 1L
-        | `YICES_ARITH_FF_CONSTANT -> 2L
-        | `YICES_BV_CONSTANT -> 3L
-        | `YICES_SCALAR_CONSTANT -> 4L
-        | `YICES_VARIABLE -> 5L
-        | `YICES_UNINTERPRETED_TERM -> 6L
-        | `YICES_ITE_TERM -> 7L
-        | `YICES_APP_TERM -> 8L
-        | `YICES_UPDATE_TERM -> 9L
-        | `YICES_TUPLE_TERM -> 10L
-        | `YICES_EQ_TERM -> 11L
-        | `YICES_DISTINCT_TERM -> 12L
-        | `YICES_FORALL_TERM -> 13L
-        | `YICES_LAMBDA_TERM -> 14L
-        | `YICES_NOT_TERM -> 15L
-        | `YICES_OR_TERM -> 16L
-        | `YICES_XOR_TERM -> 17L
-        | `YICES_BV_ARRAY -> 18L
-        | `YICES_BV_DIV -> 19L
-        | `YICES_BV_REM -> 20L
-        | `YICES_BV_SDIV -> 21L
-        | `YICES_BV_SREM -> 22L
-        | `YICES_BV_SMOD -> 23L
-        | `YICES_BV_SHL -> 24L
-        | `YICES_BV_LSHR -> 25L
-        | `YICES_BV_ASHR -> 26L
-        | `YICES_BV_GE_ATOM -> 27L
-        | `YICES_BV_SGE_ATOM -> 28L
-        | `YICES_ARITH_GE_ATOM -> 29L
-        | `YICES_ARITH_ROOT_ATOM -> 30L
-        | `YICES_ABS -> 31L
-        | `YICES_CEIL -> 32L
-        | `YICES_FLOOR -> 33L
-        | `YICES_RDIV -> 34L
-        | `YICES_IDIV -> 35L
-        | `YICES_IMOD -> 36L
-        | `YICES_IS_INT_ATOM -> 37L
-        | `YICES_DIVIDES_ATOM -> 38L
-        | `YICES_SELECT_TERM -> 39L
-        | `YICES_BIT_TERM -> 40L
-        | `YICES_BV_SUM -> 41L
-        | `YICES_ARITH_SUM -> 42L
-        | `YICES_ARITH_FF_SUM -> 43L
-        | `YICES_POWER_PRODUCT -> 44L),
-       (* function x -> (print_endline (String.concat "" ["KN int64: "; (Int64.to_string x)]) ;  *)
-       (function
-        | (-1L) -> `YICES_CONSTRUCTOR_ERROR
-        | 0L -> `YICES_BOOL_CONSTANT
-        | 1L -> `YICES_ARITH_CONSTANT
-        | 2L -> `YICES_ARITH_FF_CONSTANT
-        | 3L -> `YICES_BV_CONSTANT
-        | 4L -> `YICES_SCALAR_CONSTANT
-        | 5L -> `YICES_VARIABLE
-        | 6L -> `YICES_UNINTERPRETED_TERM
-        | 7L -> `YICES_ITE_TERM
-        | 8L -> `YICES_APP_TERM
-        | 9L -> `YICES_UPDATE_TERM
-        | 10L -> `YICES_TUPLE_TERM
-        | 11L -> `YICES_EQ_TERM
-        | 12L -> `YICES_DISTINCT_TERM
-        | 13L -> `YICES_FORALL_TERM
-        | 14L -> `YICES_LAMBDA_TERM
-        | 15L -> `YICES_NOT_TERM
-        | 16L -> `YICES_OR_TERM
-        | 17L -> `YICES_XOR_TERM
-        | 18L -> `YICES_BV_ARRAY
-        | 19L -> `YICES_BV_DIV
-        | 20L -> `YICES_BV_REM
-        | 21L -> `YICES_BV_SDIV
-        | 22L -> `YICES_BV_SREM
-        | 23L -> `YICES_BV_SMOD
-        | 24L -> `YICES_BV_SHL
-        | 25L -> `YICES_BV_LSHR
-        | 26L -> `YICES_BV_ASHR
-        | 27L -> `YICES_BV_GE_ATOM
-        | 28L -> `YICES_BV_SGE_ATOM
-        | 29L -> `YICES_ARITH_GE_ATOM
-        | 30L -> `YICES_ARITH_ROOT_ATOM
-        | 31L -> `YICES_ABS
-        | 32L -> `YICES_CEIL
-        | 33L -> `YICES_FLOOR
-        | 34L -> `YICES_RDIV
-        | 35L -> `YICES_IDIV
-        | 36L -> `YICES_IMOD
-        | 37L -> `YICES_IS_INT_ATOM
-        | 38L -> `YICES_DIVIDES_ATOM
-        | 39L -> `YICES_SELECT_TERM
-        | 40L -> `YICES_BIT_TERM
-        | 41L -> `YICES_BV_SUM
-        | 42L -> `YICES_ARITH_SUM
-        | 43L -> `YICES_ARITH_FF_SUM
-        | 44L -> `YICES_POWER_PRODUCT
-        | _ -> failwith "enum to_int") (* x) *)) in
+          | `YICES_CONSTRUCTOR_ERROR -> (-1L)
+          | `YICES_BOOL_CONSTANT -> 0L
+          | `YICES_ARITH_CONSTANT -> 1L
+          | `YICES_ARITH_FF_CONSTANT -> 2L
+          | `YICES_BV_CONSTANT -> 3L
+          | `YICES_SCALAR_CONSTANT -> 4L
+          | `YICES_VARIABLE -> 5L
+          | `YICES_UNINTERPRETED_TERM -> 6L
+          | `YICES_ITE_TERM -> 7L
+          | `YICES_APP_TERM -> 8L
+          | `YICES_UPDATE_TERM -> 9L
+          | `YICES_TUPLE_TERM -> 10L
+          | `YICES_EQ_TERM -> 11L
+          | `YICES_DISTINCT_TERM -> 12L
+          | `YICES_FORALL_TERM -> 13L
+          | `YICES_LAMBDA_TERM -> 14L
+          | `YICES_NOT_TERM -> 15L
+          | `YICES_OR_TERM -> 16L
+          | `YICES_XOR_TERM -> 17L
+          | `YICES_BV_ARRAY -> 18L
+          | `YICES_BV_DIV -> 19L
+          | `YICES_BV_REM -> 20L
+          | `YICES_BV_SDIV -> 21L
+          | `YICES_BV_SREM -> 22L
+          | `YICES_BV_SMOD -> 23L
+          | `YICES_BV_SHL -> 24L
+          | `YICES_BV_LSHR -> 25L
+          | `YICES_BV_ASHR -> 26L
+          | `YICES_BV_GE_ATOM -> 27L
+          | `YICES_BV_SGE_ATOM -> 28L
+          | `YICES_ARITH_GE_ATOM -> 29L
+          | `YICES_ARITH_ROOT_ATOM -> 30L
+          | `YICES_ABS -> 31L
+          | `YICES_CEIL -> 32L
+          | `YICES_FLOOR -> 33L
+          | `YICES_RDIV -> 34L
+          | `YICES_IDIV -> 35L
+          | `YICES_IMOD -> 36L
+          | `YICES_IS_INT_ATOM -> 37L
+          | `YICES_DIVIDES_ATOM -> 38L
+          | `YICES_SELECT_TERM -> 39L
+          | `YICES_BIT_TERM -> 40L
+          | `YICES_BV_SUM -> 41L
+          | `YICES_ARITH_SUM -> 42L
+          | `YICES_ARITH_FF_SUM -> 43L
+          | `YICES_POWER_PRODUCT -> 44L),
+         (function
+          | (-1L) -> `YICES_CONSTRUCTOR_ERROR
+          | 0L -> `YICES_BOOL_CONSTANT
+          | 1L -> `YICES_ARITH_CONSTANT
+          | 2L -> `YICES_ARITH_FF_CONSTANT
+          | 3L -> `YICES_BV_CONSTANT
+          | 4L -> `YICES_SCALAR_CONSTANT
+          | 5L -> `YICES_VARIABLE
+          | 6L -> `YICES_UNINTERPRETED_TERM
+          | 7L -> `YICES_ITE_TERM
+          | 8L -> `YICES_APP_TERM
+          | 9L -> `YICES_UPDATE_TERM
+          | 10L -> `YICES_TUPLE_TERM
+          | 11L -> `YICES_EQ_TERM
+          | 12L -> `YICES_DISTINCT_TERM
+          | 13L -> `YICES_FORALL_TERM
+          | 14L -> `YICES_LAMBDA_TERM
+          | 15L -> `YICES_NOT_TERM
+          | 16L -> `YICES_OR_TERM
+          | 17L -> `YICES_XOR_TERM
+          | 18L -> `YICES_BV_ARRAY
+          | 19L -> `YICES_BV_DIV
+          | 20L -> `YICES_BV_REM
+          | 21L -> `YICES_BV_SDIV
+          | 22L -> `YICES_BV_SREM
+          | 23L -> `YICES_BV_SMOD
+          | 24L -> `YICES_BV_SHL
+          | 25L -> `YICES_BV_LSHR
+          | 26L -> `YICES_BV_ASHR
+          | 27L -> `YICES_BV_GE_ATOM
+          | 28L -> `YICES_BV_SGE_ATOM
+          | 29L -> `YICES_ARITH_GE_ATOM
+          | 30L -> `YICES_ARITH_ROOT_ATOM
+          | 31L -> `YICES_ABS
+          | 32L -> `YICES_CEIL
+          | 33L -> `YICES_FLOOR
+          | 34L -> `YICES_RDIV
+          | 35L -> `YICES_IDIV
+          | 36L -> `YICES_IMOD
+          | 37L -> `YICES_IS_INT_ATOM
+          | 38L -> `YICES_DIVIDES_ATOM
+          | 39L -> `YICES_SELECT_TERM
+          | 40L -> `YICES_BIT_TERM
+          | 41L -> `YICES_BV_SUM
+          | 42L -> `YICES_ARITH_SUM
+          | 43L -> `YICES_ARITH_FF_SUM
+          | 44L -> `YICES_POWER_PRODUCT
+          | _ -> failwith "enum to_int"))
+    in
     object
       method ctype = sint
       method to_int = to_int
       method of_int = of_int
     end
+  [%%else]
+  let term_constructor =
+   (* prerr_endline "[ctypes_type_description.ml] term_constructor" ; *)
+    let (to_int, of_int) =
+      ((function
+          | `YICES_CONSTRUCTOR_ERROR -> (-1L)
+          | `YICES_ARITH_FF_CONSTANT -> (-2L)
+          | `YICES_ARITH_FF_SUM -> (-3L)
+          | `YICES_BOOL_CONSTANT -> 0L
+          | `YICES_ARITH_CONSTANT -> 1L
+          | `YICES_BV_CONSTANT -> 2L
+          | `YICES_SCALAR_CONSTANT -> 3L
+          | `YICES_VARIABLE -> 4L
+          | `YICES_UNINTERPRETED_TERM -> 5L
+          | `YICES_ITE_TERM -> 6L
+          | `YICES_APP_TERM -> 7L
+          | `YICES_UPDATE_TERM -> 8L
+          | `YICES_TUPLE_TERM -> 9L
+          | `YICES_EQ_TERM -> 10L
+          | `YICES_DISTINCT_TERM -> 11L
+          | `YICES_FORALL_TERM -> 12L
+          | `YICES_LAMBDA_TERM -> 13L
+          | `YICES_NOT_TERM -> 14L
+          | `YICES_OR_TERM -> 15L
+          | `YICES_XOR_TERM -> 16L
+          | `YICES_BV_ARRAY -> 17L
+          | `YICES_BV_DIV -> 18L
+          | `YICES_BV_REM -> 19L
+          | `YICES_BV_SDIV -> 20L
+          | `YICES_BV_SREM -> 21L
+          | `YICES_BV_SMOD -> 22L
+          | `YICES_BV_SHL -> 23L
+          | `YICES_BV_LSHR -> 24L
+          | `YICES_BV_ASHR -> 25L
+          | `YICES_BV_GE_ATOM -> 26L
+          | `YICES_BV_SGE_ATOM -> 27L
+          | `YICES_ARITH_GE_ATOM -> 28L
+          | `YICES_ARITH_ROOT_ATOM -> 29L
+          | `YICES_ABS -> 30L
+          | `YICES_CEIL -> 31L
+          | `YICES_FLOOR -> 32L
+          | `YICES_RDIV -> 33L
+          | `YICES_IDIV -> 34L
+          | `YICES_IMOD -> 35L
+          | `YICES_IS_INT_ATOM -> 36L
+          | `YICES_DIVIDES_ATOM -> 37L
+          | `YICES_SELECT_TERM -> 38L
+          | `YICES_BIT_TERM -> 39L
+          | `YICES_BV_SUM -> 40L
+          | `YICES_ARITH_SUM -> 41L
+          | `YICES_POWER_PRODUCT -> 42L),
+         (function
+          | (-1L) -> `YICES_CONSTRUCTOR_ERROR
+          | 0L -> `YICES_BOOL_CONSTANT
+          | 1L -> `YICES_ARITH_CONSTANT
+          | 2L -> `YICES_BV_CONSTANT
+          | 3L -> `YICES_SCALAR_CONSTANT
+          | 4L -> `YICES_VARIABLE
+          | 5L -> `YICES_UNINTERPRETED_TERM
+          | 6L -> `YICES_ITE_TERM
+          | 7L -> `YICES_APP_TERM
+          | 8L -> `YICES_UPDATE_TERM
+          | 9L -> `YICES_TUPLE_TERM
+          | 10L -> `YICES_EQ_TERM
+          | 11L -> `YICES_DISTINCT_TERM
+          | 12L -> `YICES_FORALL_TERM
+          | 13L -> `YICES_LAMBDA_TERM
+          | 14L -> `YICES_NOT_TERM
+          | 15L -> `YICES_OR_TERM
+          | 16L -> `YICES_XOR_TERM
+          | 17L -> `YICES_BV_ARRAY
+          | 18L -> `YICES_BV_DIV
+          | 19L -> `YICES_BV_REM
+          | 20L -> `YICES_BV_SDIV
+          | 21L -> `YICES_BV_SREM
+          | 22L -> `YICES_BV_SMOD
+          | 23L -> `YICES_BV_SHL
+          | 24L -> `YICES_BV_LSHR
+          | 25L -> `YICES_BV_ASHR
+          | 26L -> `YICES_BV_GE_ATOM
+          | 27L -> `YICES_BV_SGE_ATOM
+          | 28L -> `YICES_ARITH_GE_ATOM
+          | 29L -> `YICES_ARITH_ROOT_ATOM
+          | 30L -> `YICES_ABS
+          | 31L -> `YICES_CEIL
+          | 32L -> `YICES_FLOOR
+          | 33L -> `YICES_RDIV
+          | 34L -> `YICES_IDIV
+          | 35L -> `YICES_IMOD
+          | 36L -> `YICES_IS_INT_ATOM
+          | 37L -> `YICES_DIVIDES_ATOM
+          | 38L -> `YICES_SELECT_TERM
+          | 39L -> `YICES_BIT_TERM
+          | 40L -> `YICES_BV_SUM
+          | 41L -> `YICES_ARITH_SUM
+          | 42L -> `YICES_POWER_PRODUCT
+          | _ -> failwith "enum to_int"))
+    in
+    object
+      method ctype = sint
+      method to_int = to_int
+      method of_int = of_int
+    end
+  [%%endif]
   let term_constructor_t =
     typedef term_constructor#ctype "term_constructor_t"
 
--- src_config/discover.ml
+++ src_config/discover.ml
@@ -11,10 +11,13 @@ let () =
   let sys = ref "" in
   let pkg = ref [] in
   let first = ref "" in
+  let optcomp_cookie_file = ref None in
   let args =
     Arg.(
     [ ("-system", Set_string sys, "set system");
-      ("-pkg", Tuple[Set_string first; String(fun s -> pkg := (!first,s)::!pkg)], "package dependency")
+      ("-pkg", Tuple[Set_string first; String(fun s -> pkg := (!first,s)::!pkg)], "package dependency");
+      ("-optcomp-cookie", String (fun s -> optcomp_cookie_file := Some s),
+       "emit a lines file with ppx_optcomp cookie based on Yices version")
     ])
   in
   C.main ~args ~name:"gmp" @@
@@ -54,4 +57,36 @@ let () =
 
     C.Flags.write_sexp "c_library_flags.sexp" conf.libs;
 
-    C.Flags.write_lines "c_flags.lines" conf.cflags
+    C.Flags.write_lines "c_flags.lines" conf.cflags;
+
+    begin
+      match !optcomp_cookie_file with
+      | None -> ()
+      | Some fname ->
+          let open C.C_define in
+          let defs =
+            import c ~c_flags:conf.cflags ~includes:["yices.h"]
+              [ ("__YICES_VERSION", Int);
+                ("__YICES_VERSION_MAJOR", Int);
+                ("__YICES_VERSION_PATCHLEVEL", Int)
+              ]
+          in
+          let find_int name =
+            match List.assoc_opt name defs with
+            | Some (Value.Int v) -> v
+            | _ -> C.die "Missing or non-int %s from yices.h" name
+          in
+          let v_major = find_int "__YICES_VERSION" in
+          let v_minor = find_int "__YICES_VERSION_MAJOR" in
+          let ge_2_7 =
+            v_major > 2 || (v_major = 2 && v_minor >= 7)
+          in
+          let env_expr =
+            if ge_2_7 then
+              "env~yices_ge_2_7:(Defined(true))"
+            else
+              "env~yices_ge_2_7:Undefined"
+          in
+          C.Flags.write_lines fname
+            [ "-cookie"; "ppx_optcomp.env=" ^ env_expr ]
+    end
