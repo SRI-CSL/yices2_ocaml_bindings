f15312f93e3810064d8c2348a42f890928df41f1
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2026-01-13 23:26:26 -0800

    Silence missing install warnings on uninstall

--- scripts/dune_uninstall_quiet.sh
+++ scripts/dune_uninstall_quiet.sh
@@ -23,7 +23,7 @@ if ! dune uninstall 2> "${tmp_log}"; then
 fi
 
 filtered_log="$(mktemp)"
-grep -v -E "Directory .* is not empty|cannot delete \\(ignoring\\)\\.|delete \\(ignoring\\)\\.|cannot be installed because they do not exist|^Error: The following files which are listed in .*\\.install|^- _build/install/" "${tmp_log}" > "${filtered_log}" || true
+grep -v -E "Directory .* is not empty|cannot delete \(ignoring\)\.|delete \(ignoring\)\.|cannot be installed because they do not exist|^Error: The following files which are listed in .*\.install|^Error: The following <package>\.install are missing|^- _build/install/|^- _build/.+\.install$|^Hint: try running \'dune build \\[\-p <pkg>\\] @install\'" "${tmp_log}" > "${filtered_log}" || true
 
 if [ "$status" -ne 0 ]; then
   if [ -s "${filtered_log}" ]; then
