af3e0dcabf85a1ec29d7d65eeb20439e01aad822
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2026-06-03 14:43:46 -0700

    Lower sigalt stress default iterations; Supported by Codex/GPT5.5

--- src_tests/sigalt_freevar_stress.ml
+++ src_tests/sigalt_freevar_stress.ml
@@ -7,8 +7,8 @@ module Yices = Make(NoErrorHandling)
 
 let stress_iters () =
   match Sys.getenv_opt "YICES_SIGALT_STRESS_ITERS" with
-  | Some s -> (try int_of_string s with _ -> 200000)
-  | None -> 200000
+  | Some s -> (try int_of_string s with _ -> 20000)
+  | None -> 20000
 
 let gc_interval () =
   match Sys.getenv_opt "YICES_SIGALT_GC_INTERVAL" with
